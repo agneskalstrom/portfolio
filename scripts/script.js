39a20274b9d08e8a6c5e1f57e7ade4a71694355c
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2020-11-12 15:34:12 +0100

    Commit all files

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const toggle = document.getElementById("toggle");
+const pageStyle = document.getElementById("pagestyle");
+
+if (localStorage.getItem("lastActiveSheet")) {
+    pageStyle.setAttribute("href", localStorage.getItem("lastActiveSheet"));
+}
+
+toggle.addEventListener("click", swapStyleSheet);
+
+function swapStyleSheet() {
+    const style = "style";
+    const invert = "invert";
+    pageStyle.x = invert == pageStyle.x ? style : invert;
+    pageStyle.href = './styles/' + pageStyle.x + '.css';
+    let selectedSheet = pageStyle.getAttribute("href");
+    localStorage.setItem("lastActiveSheet", selectedSheet);
+    console.log(selectedSheet);
+}
